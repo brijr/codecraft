c12e889d3dcb308c5c76b88dcfe32506c6d30f9c
Bridger Tower <bridgertower@gmail.com>
2023-01-11 14:47:42 -0700

    Updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-neutral-200 dark:bg-neutral-800 dark:text-white lg:w-1/2 rounded-lg p-6">
+    <footer className="bg-neutral-100 dark:bg-neutral-800 dark:text-white lg:w-1/2 rounded-lg p-6">
       <h3 className="mt-0">Thanks for Visiting Code Craft ©</h3>
       <p>Created by <a href="https://btower.dev">Bridger Tower</a>. Circa 2023</p>
       <a href="https://www.youtube.com/@code-craft">--{'>'} Watch on YouTube</a>
