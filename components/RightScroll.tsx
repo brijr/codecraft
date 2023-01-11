2b0c570a41cdf68ee7f802764c5f38e1bf73bef2
Bridger Tower <bridgertower@gmail.com>
2023-01-11 15:05:41 -0700

    Updated

--- components/RightScroll.tsx
+++ components/RightScroll.tsx
@@ -3,7 +3,7 @@ export default function RightScroll() {
     <aside className="bg-neutral-100 static lg:fixed min-h-screen top-0 right-0 dark:bg-neutral-800 dark:text-white lg:w-1/3">
       <div className="p-6">
         <h3 className="mt-0">Recent Videos</h3>
-        <p>Created by <a href="https://btower.dev">Bridger Tower</a>. Circa 2023</p>
+        <p>Coming soon . . .</p>
       </div>
     </aside>
   )
