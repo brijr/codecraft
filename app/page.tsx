017f35a11dadeafe6b7b39640efdd04c8ba17124
Bridger Tower <bridgertower@gmail.com>
2023-01-11 15:05:06 -0700

    Updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,38 @@
 import Footer from "../components/Footer";
+import RightScroll from "../components/RightScroll";
 
 export default function Page() {
   return (
     <main className="p-6">
-      <div className="mb-24 lg:w-1/2">
-        <h1 className="text-6xl">
-          Welcome to Code Craft © Where designers learn to code.
-        </h1>
-        <div className="flex flex-col my-8">
-          <a href="https://www.youtube.com/@code-craft">
-            --{">"} Watch on YouTube
-          </a>
-          <a href="https://www.github.com/brijr/codecraft">
-            --{">"} Star on GitHub
-          </a>
+      <div className="lg:flex">
+        <div className="mb-24 lg:w-1/2">
+          <h1 className="text-6xl">
+            Welcome to Code Craft © Where designers learn to code.
+          </h1>
+          <div className="flex flex-col my-8">
+            <a href="https://www.youtube.com/@code-craft">
+              --{">"} Watch on YouTube
+            </a>
+            <a href="https://www.github.com/brijr/codecraft">
+              --{">"} Star on GitHub
+            </a>
+          </div>
+          <h3>Resources for Designers and Developers</h3>
+          <p>
+            Are you a designer looking to add coding skills to your toolkit?
+            Look no further than Code Craft - the YouTube channel that teaches
+            coding to designers. Our channel is full of helpful coding tutorials
+            for designers of all levels, from beginners to experienced coders.
+            We cover topics from the basics of HTML and CSS to more advanced
+            topics like React and NextJS. Don&apos;t be intimidated if coding
+            seems daunting - our tutorials are easy to follow and we break down
+            the concepts into easy-to-understand chunks. So come along and join
+            us on our coding journey - you won&apos;t regret it!
+          </p>
+          <Footer />
         </div>
-        <p>
-          Are you a designer looking to add coding skills to your toolkit? Look
-          no further than Code Craft - the YouTube channel that teaches coding
-          to designers. Our channel is full of helpful coding tutorials for
-          designers of all levels, from beginners to experienced coders. We
-          cover topics from the basics of HTML and CSS to more advanced topics
-          like React and NextJS. Don&apos;t be intimidated if coding seems
-          daunting - our tutorials are easy to follow and we break down the
-          concepts into easy-to-understand chunks. So come along and join us on
-          our coding journey - you won&apos;t regret it!
-        </p>
+        <RightScroll />
       </div>
-      <Footer />
     </main>
   );
 }
---
+++ components/RightScroll.tsx
@@ -0,0 +1,10 @@
+export default function RightScroll() {
+  return (
+    <aside className="bg-neutral-100 static lg:fixed min-h-screen top-0 right-0 dark:bg-neutral-800 dark:text-white lg:w-1/3">
+      <div className="p-6">
+        <h3 className="mt-0">Recent Videos</h3>
+        <p>Created by <a href="https://btower.dev">Bridger Tower</a>. Circa 2023</p>
+      </div>
+    </aside>
+  )
+}
